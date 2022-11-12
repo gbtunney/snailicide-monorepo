baed2c15e0a02c1f6ec7be5ec6e0d41333798e83
Gillian Tunney <gbtunney@mac.com>
2022-11-12 02:58:39 -0500

    refactor(root): updated root to work with build-config library changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,8 @@
 /* eslint-env node */
 
 /** @file Local package eslint config file */
-const options = require('@snailicide/build-config/.eslintrc.js')
+const options = require('@snailicide/build-config/eslint')
+
 module.exports = {
     ...options,
-    rules: {
-        'no-undef': 'warn',
-    },
 }
