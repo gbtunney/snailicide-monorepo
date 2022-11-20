24d057af56ca008de996b51071670c10bdcc52bd
Gillian Tunney <gbtunney@mac.com>
2022-11-20 02:48:41 -0500

    build(root): added release to commitlint scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'scope-enum': [
             2,
             'always',
-            ['root', 'build-config', 'template-cli', 'g-library'],
+            ['root', 'release', 'build-config', 'template-cli', 'g-library'],
         ],
     },
 }
