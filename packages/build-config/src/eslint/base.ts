9a75da51dc9a872d47a7d63f3538830baf088e82
Gillian Tunney <gbtunney@mac.com>
2024-07-02 10:12:23 -0400

    feat(build-config): stripped unused eslint plugins

--- packages/build-config/src/eslint/base.ts
+++ packages/build-config/src/eslint/base.ts
@@ -10,59 +10,6 @@ import type { Linter } from 'eslint'
 
 const options: Linter.BaseConfig = {
     // root: true,
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2021,
-    },
-    plugins: ['eslint-plugin-import', 'filenames-simple'],
-    extends: [
-        'eslint:recommended',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        //"plugin:filenames-simple/recommended"       // for pure ECMAScript/TypeScript project
-        //"plugin:filenames-simple/recommended-react" // for React.js project
-        //"plugin:filenames-simple/recommended-vue"   // for Vue.js project
-    ],
-    rules: {
-        'import/no-unresolved': 'error',
-        'filenames-simple/extension': 'error',
-        'filenames-simple/naming-convention': [
-            'warn',
-            {
-                rule: 'camelCase',
-                excepts: ['.eslintrc.js'],
-            },
-        ],
-        //'@typescript-eslint/no-var-requires': 'off',
-    },
-    settings: {
-        'filenames-simple': {
-            //  allowedExtensions: ['.eslintrc.js'],
-        },
-        /* * I have no idea what this means, i literally copied it from [eslint-import-resolver-typescript - npm](https://www.npmjs.com/package/eslint-import-resolver-typescript) * */
-        'import/extensions': ['.js', '.jsx', '.ts'],
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-        'import/resolver': {
-            typescript: {
-                alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-                // Multiple tsconfigs (Useful for monorepos)
-                // use a glob pattern
-                project: 'packages/*/tsconfig.json',
-            },
-        },
-    },
-    overrides: [
-        {
-            files: ['.eslintrc.js', '**/?(*.)+(spec|test).[jt]s?(x)'],
-            rules: {
-                'filenames-simple/naming-convention': 'off',
-            },
-        },
-    ],
 }
 export const baseOptions = options
 export default options
