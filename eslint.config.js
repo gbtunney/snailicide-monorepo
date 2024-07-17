7f1dc2203a4b19acbfd8fdab06f4122972de16b5
Gillian Tunney <gbtunney@mac.com>
2024-07-17 01:30:37 -0400

    style(root): ignore doccs in prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,9 @@ import { EsLint } from '@snailicide/build-config'
 
 const FLAT_CONFIG = await EsLint.flatConfig()
 
-export default [...FLAT_CONFIG]
+export default [
+    ...FLAT_CONFIG,
+    {
+        ignores: ['packages/**/docs/**/*'],
+    },
+]
