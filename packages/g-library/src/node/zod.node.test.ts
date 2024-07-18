79ffcaebd0f2238997c4dc9a276633a48ca6268a
Gillian Tunney <gbtunney@mac.com>
2024-07-18 18:32:37 -0400

    test(g-library): renamed node zod tests with fspaths

--- packages/g-library/src/node/zod.node.test.ts
+++ packages/g-library/src/node/zod.node.test.ts
@@ -1,10 +1,12 @@
 import { describe, expect, test } from 'vitest'
 
+import * as zod from './zod.node.js'
+
 describe('zod custom schemas', () => {
     test('TODO: reenable', () => {
         expect(true).toBe(true)
         //todo: reenable
-        /*  expect(
+        expect(
             zod
                 .fsPathTypeExists('any', './')
                 .safeParse('sample_image/testimage.jpeg').success,
@@ -15,7 +17,7 @@ describe('zod custom schemas', () => {
                 .safeParse('sample_image/testimage.jpeg').success,
         ).toBe(false)
         expect(
-            zod.fsPathTypeExists('none', './').safeParse('sample_image/!*.png')
+            zod.fsPathTypeExists('none', './').safeParse('sample_image/*.png')
                 .success,
         ).toBe(true)
         expect(
@@ -24,15 +26,15 @@ describe('zod custom schemas', () => {
         ).toBe(true)
 
         expect(
-            zod.fsPathTypeExists('any').safeParse('./sample_image/!*').success,
+            zod.fsPathTypeExists('any').safeParse('./sample_image/*').success,
         ).toBe(true)
         expect(
-            zod.fsPathTypeExists('any').safeParse('./sample_imaddge/!*').success,
+            zod.fsPathTypeExists('any').safeParse('./sample_imaddge/*').success,
         ).toBe(false)
         expect(
             zod
                 .fsPathTypeExists(['glob', 'directory'])
-                .safeParse('./sample_image/!*').success,
+                .safeParse('./sample_image/*').success,
         ).toBe(true)
         expect(
             zod
@@ -42,7 +44,7 @@ describe('zod custom schemas', () => {
         expect(
             zod
                 .fsPathArrayHasFiles(false, './')
-                .safeParse('sample_image/!*.jpeg').success,
+                .safeParse('sample_image/*.jpeg').success,
         ).toBe(true)
         expect(
             zod.fsPathArrayHasFiles(false, './').safeParse('sample_image')
@@ -60,7 +62,7 @@ describe('zod custom schemas', () => {
 
         expect(
             zod.fsPathExists(false, './').safeParse('sample_image22').success,
-        ).toBe(true)*/
+        ).toBe(true)
     })
 })
 export {}
