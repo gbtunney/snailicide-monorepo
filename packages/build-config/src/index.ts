d5657f6c251e61f702d3f8b0f956f7fa0e0c1d6f
Gillian Tunney <gbtunney@mac.com>
2024-08-06 20:45:44 -0400

    feat(build-config): added commitlint config

--- packages/build-config/src/index.ts
+++ packages/build-config/src/index.ts
@@ -1,28 +1,30 @@
+/* *     COMMITLINT CONFIG and COMMITIZEN * */
+export { commitlint } from './commitlint/index.js'
 /* * ESLINT * */
 export { EsLint } from './eslint/index.js'
-export type { EslintConfig } from './eslint/index.js'
 
+export type { EslintConfig } from './eslint/index.js'
 /* * NPM UTILITIES * */
 export { npm } from './npm/index.js'
-export type { BasePackage, PackageJson, PackageJsonInput } from './npm/index.js'
 
+export type { BasePackage, PackageJson, PackageJsonInput } from './npm/index.js'
 /* * PRETTIER * */
 export { Prettier } from './prettier/index.js'
-export type { PrettierConfig, PrettierOptions } from './prettier/index.js'
 
+export type { PrettierConfig, PrettierOptions } from './prettier/index.js'
 /* * ROLLUP * */
 export { rollup } from './rollup/index.js'
+
 export type {
     ConfigOptions,
     EntryConfig,
     ExportType,
     PluginKey,
 } from './rollup/index.js'
-
 /** TYPEDOC */
 export { typedoc } from './typedoc/index.js'
-export type { TypedocConfig, TypedocMarkdownConfig } from './typedoc/index.js'
 
+export type { TypedocConfig, TypedocMarkdownConfig } from './typedoc/index.js'
 /* *  UTILITIES *  */
 export { exportJSON, isPlainObject, safeDeserializeJSON } from './utilities.js'
 
@@ -31,4 +33,5 @@ export { vite } from './vite/index.js'
 
 /* * VITEST CONFIG * */
 export { vitest } from './vitest/index.js'
+
 export { merge } from 'ts-deepmerge'
