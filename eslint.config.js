0c1057250de3db0f9e50309118e12fcfedf885d7
Gillian Tunney <gbtunney@mac.com>
2024-08-31 20:37:28 -0400

    feat(root): new eslint config, added vitepress to ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,39 @@
-import { EsLint } from '@snailicide/build-config'
-import tseslint from 'typescript-eslint'
-import url from 'node:url'
+import { EsLint } from "@snailicide/build-config"
+import tseslint from "typescript-eslint"
+import url from "node:url"
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 const FLAT_CONFIG = await EsLint.flatConfig(__dirname)
 
 export default [
     ...FLAT_CONFIG,
     {
-        ignores: ['packages/**/docs/**/*', '.history/**/*'],
+        ignores: [
+            "packages/**/docs/**/*",
+            ".history/**/*",
+            "**/.vitepress/cache",
+        ],
     },
     ...tseslint.config({
         extends: [tseslint.configs.disableTypeChecked],
         files: [
-            'packages/cli-template/**/*',
-            'packages/g-shopify-library/**/*',
-            'packages/g-windi/**/*',
-            'packages/vite-plugin-shopify-liquid-modules/**/*',
-            'packages/vite-plugin-shopify-theme-schema/**/*',
+            "packages/cli-template/**/*",
+            "packages/g-shopify-library/**/*",
+            "packages/g-windi/**/*",
+            "packages/vite-plugin-shopify-liquid-modules/**/*",
+            "packages/vite-plugin-shopify-theme-schema/**/*",
         ],
         rules: {
-            '@typescript-eslint/explicit-function-return-type': 'warn',
+            "@typescript-eslint/explicit-function-return-type": "warn",
             //todo: this needs to ditch the typechecked thing for now
-            '@typescript-eslint/naming-convention': 'off',
-            '@typescript-eslint/no-floating-promises': 'off',
+            "@typescript-eslint/naming-convention": "off",
+            "@typescript-eslint/no-floating-promises": "off",
         },
     }),
     ...tseslint.config({
-        files: ['packages/g-shopify-library/**/*'],
+        files: ["packages/g-shopify-library/**/*"],
         rules: {
-            'filenames-simple/naming-convention': 'warn',
+            "filenames-simple/naming-convention": "warn",
         },
     }),
     {},
