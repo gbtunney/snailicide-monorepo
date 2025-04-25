3b3687b60322d08369e15778141c54b116c2eefb
Gillian Tunney <gbtunney@mac.com>
2025-04-25 01:16:51 -0400

    chore(root,g-library,g-windi,cli-app): updated deps in all

--- packages/g-library/typedoc.config.ts
+++ packages/g-library/typedoc.config.ts
@@ -4,6 +4,8 @@ import url from 'node:url'
 import path from 'path'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 //import { Options as OptionsVitepress }from 'typedoc-vitepress-theme'
+
+/** @todo: needs to be updated to vitepress */
 const custom: Partial<TypeDocOptions> = {
     //  categorizeByGroup: true,
     entryPoints: [path.resolve(`${__dirname}/src/node.ts`)],
