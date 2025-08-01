8fb4f858c4206b160234192e5cf68814e00b4c81
Gillian Tunney <gbtunney@mac.com>
2025-08-01 01:26:34 -0400

    fix(cli-app): fixed flakey tests, updated packages to have nx

--- packages/cli-app/cli.test.ts
+++ packages/cli-app/cli.test.ts
@@ -1,39 +1,55 @@
-import shell from 'shelljs'
+import { execa } from 'execa'
 import { describe, expect, test } from 'vitest'
 import pkg from './package.json'
 import { parsePackageJson } from './types/index.js'
+
 describe('CLI-app example', () => {
-    test('should match example version and not error', () => {
-        const result = shell.exec(
-            'node ./dist/example.js --interactive false -v',
-            { silent: true },
-        )
-        expect(result.stdout).toMatch(/0\.0\.0/)
-        expect(result.code).toBe(0)
+    test('should match example version and not error', async () => {
+        const { exitCode, stdout } = await execa('node', [
+            './dist/example.js',
+            '--interactive',
+            'false',
+            '-v',
+        ])
+        expect(stdout).toMatch(/0\.0\.0/)
+        expect(exitCode).toBe(0)
     })
 
-    test('error if no arguments', () => {
-        const result = shell.exec(
-            'node ./dist/example.js --interactive false',
-            { silent: true },
+    test('error if no arguments', async () => {
+        const { exitCode } = await execa(
+            'node',
+            ['./dist/example.js', '--interactive', 'false'],
+            { reject: false },
         )
-        expect(result.code).toBe(1)
+        expect(exitCode).toBe(1)
     })
 
-    test('good if required  argument present', () => {
-        const result = shell.exec(
-            'node ./dist/example.js  --outDir "./dist" --interactive false  ',
-            { silent: true },
-        )
-        expect(result.code).toBe(0)
+    test('good if required  argument present', async () => {
+        const { exitCode } = await execa('node', [
+            './dist/example.js',
+            '--outDir',
+            './dist',
+            '--interactive',
+            'false',
+        ])
+        expect(exitCode).toBe(0)
     })
 
-    test('error  if invalid root directory', () => {
-        const result = shell.exec(
-            'node ./dist/example.js  -o "./dist" -r "./missingD" --interactive false',
-            { silent: true },
+    test('error  if invalid root directory', async () => {
+        const { exitCode } = await execa(
+            'node',
+            [
+                './dist/example.js',
+                '-o',
+                './dist',
+                '-r',
+                './missingD',
+                '--interactive',
+                'false',
+            ],
+            { reject: false },
         )
-        expect(result.code).toBe(1)
+        expect(exitCode).toBe(1)
     })
 })
 
@@ -42,7 +58,6 @@ describe('CLI-app package helper', () => {
         expect(parsePackageJson(pkg)).toBeDefined()
     })
 
-    //TODO:fix
     test.skip('semver error', () => {
         expect(
             parsePackageJson({ ...pkg, version: '1.2.2.2.2' }),
