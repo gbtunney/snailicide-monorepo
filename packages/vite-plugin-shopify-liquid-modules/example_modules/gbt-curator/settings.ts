571781cb06ad5dc51d597b02e4cbd1217abe06b8
Gillian Tunney <gbtunney@mac.com>
2025-11-05 19:50:25 -0500

    fix(vite-shopify-modules): fixed error with curator publish_id

--- packages/vite-plugin-shopify-liquid-modules/example_modules/gbt-curator/settings.ts
+++ packages/vite-plugin-shopify-liquid-modules/example_modules/gbt-curator/settings.ts
@@ -10,10 +10,8 @@ const getSettings = () => {
     // defineSettings(headingSettings, "heading_") as PrefixProperties<HeadingSettings, "heading_">
     const curatorSettings: SettingsMapped<CuratorGroupSettings> = [
         {
-            default: '8f06d909-ac15-451b-a5b8-0a275b7a44f5',
             id: 'publish_id',
             label: 'Curator Publish ID ( from embed code )',
-            type: 'text',
         },
     ]
 
