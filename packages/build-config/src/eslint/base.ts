8ad3b0af4ec8cba5efb1249da34ec488f4a9c5d2
Gillian Tunney <gbtunney@mac.com>
2025-11-17 06:17:11 -0500

    fix(build-config): eslint project serviice]

--- packages/build-config/src/eslint/base.ts
+++ packages/build-config/src/eslint/base.ts
@@ -44,7 +44,7 @@ export const flatEslintConfig = async (__dirname: string): Promise<Config> => {
             languageOptions: {
                 globals: { ...globals.browser, ...globals.node },
                 parserOptions: {
-                    project: true,
+                    // project: true, // remove: incompatible with projectService in @typescript-eslint v7
                     projectService: true,
                     tsconfigRootDir: __dirname,
                 },
